26e12569e9e403d21edfc105df41ee89963f69f4
Mustafa <mmermmus123@gmail.com>
2022-11-13 11:02:18 +0300

    testing the problem on displaying lastpage elements

--- src/test.cpp
+++ src/test.cpp
@@ -26,8 +26,25 @@ int main()
     TabBar tb = TabBar();
     LocationArrow la = LocationArrow();
     SLB sb = SLB();
+    int a = 4;
+    try
+    {
+        cout << *sb.findAdress(a, 1, content->YoneticiList) << "  " << content->contentAvarageCalculator(sb.findAdress(a, 1, content->YoneticiList)) << endl;
+        cout << *sb.findAdress(a, 2, content->YoneticiList) << "  " << content->contentAvarageCalculator(sb.findAdress(a, 2, content->YoneticiList)) << endl;
+        cout << *sb.findAdress(a, 3, content->YoneticiList) << "  " << content->contentAvarageCalculator(sb.findAdress(a, 3, content->YoneticiList)) << endl;
+        cout << *sb.findAdress(a, 4, content->YoneticiList) << "  " << content->contentAvarageCalculator(sb.findAdress(a, 4, content->YoneticiList)) << endl;
+        cout << *sb.findAdress(a, 5, content->YoneticiList) << "  " << content->contentAvarageCalculator(sb.findAdress(a, 5, content->YoneticiList)) << endl;
+        cout << *sb.findAdress(a, 6, content->YoneticiList) << "  " << content->contentAvarageCalculator(sb.findAdress(a, 6, content->YoneticiList)) << endl;
+        cout << *sb.findAdress(a, 7, content->YoneticiList) << "  " << content->contentAvarageCalculator(sb.findAdress(a, 7, content->YoneticiList)) << endl;
+        cout << *sb.findAdress(a, 8, content->YoneticiList) << "  " << content->contentAvarageCalculator(sb.findAdress(a, 8, content->YoneticiList)) << endl;
+        cout << *sb.findAdress(a, 9, content->YoneticiList) << "  " << content->contentAvarageCalculator(sb.findAdress(a, 9, content->YoneticiList)) << endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-    content->writingAllElements();
+    // content->writingAllElements();
     content->~Content();
     converter->~Converter();
     delete content;
